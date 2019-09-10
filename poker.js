1da1ec6108bbe79caef65a6cb58789ccccf79070
Brandon Rowe <brandonhrowe@gmail.com>
2019-09-10 13:43:25 -0400

    deal method added

--- poker.js
+++ poker.js
@@ -12,14 +12,36 @@ class Poker {
       );
       const deck = await response.json();
       this.deck = deck.deck_id;
-      console.log(this)
+      console.log(this);
     } catch (error) {
       console.log(error);
     }
   }
 
+  async deal(cards = 5) {
+    try {
+      const response = await fetch(
+        `https://deckofcardsapi.com/api/deck/${
+          this.deck ? this.deck : "new"
+        }/draw/?count=${cards}`
+      );
+      const hand = await response.json();
+      if (!this.deck) {
+        this.deck = hand.deck_id;
+      }
+      this.hand = hand.cards;
+      console.log(this);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 const pokerHand = new Poker();
 
-pokerHand.shuffle(1);
+const func = async game => {
+  await game.shuffle();
+  await game.deal();
+};
+
+func(pokerHand);
